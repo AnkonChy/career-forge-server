c5290c6af003f29eb775a9f41d7e4da8b01a3036
Ankonchy <ankonchy1@gmail.com>
2026-09-05 01:24:12 -0700

    cors functionality updated

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,27 @@ import resumeRoutes from "./routes/resumeRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
+// Allowed origins list
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://career-forge-client.vercel.app",
+];
 
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: function (origin, callback) {
+      // Postman/curl/server-to-server requests e origin thake na
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 app.use(express.json());
